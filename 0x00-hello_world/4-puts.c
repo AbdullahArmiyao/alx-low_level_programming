b596d37d2872bb33aa03d5679c39f86abe960cfb
AbdullahArmiyao <abdulbnarmi@gmail.com>
2023-09-07 05:35:03 -0700

    First C File

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/* 
+ * main - prints text
+ * puts is used in place of prinf
+ * return value of 0
+ */
+int main(void){
+  puts("\"Programming is like building a multilingual  puzzle\n");
+  return 0;
+}
