efe48f5ce32239e2d71419d27adafb830a0f28af
AbdullahArmiyao <abdulbnarmi@gmail.com>
2023-09-07 06:59:16 -0700

    printing the size of each data type

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main -main function
+ * Return: (0) success
+ */
+
+int main(void)
+{
+	printf("Size of int: %lu bytes, Size of float: %lu bytes, Size of double: %lu bytes\n", sizeof(int), sizeof(float), sizeof(double));
+	return (0);
+}
+
