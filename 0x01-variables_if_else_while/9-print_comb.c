50544a55571a147f2d31d7acf6ed193ab59c708b
AbdullahArmiyao <abdulbnarmi@gmail.com>
2023-09-08 12:27:07 -0700

    printing in pairs

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,20 +8,14 @@
 
 int main(void)
 {
-	int d1 = 0, d2 = 0;
+	int d1 = 0;
 
 	while (d1 <= 9)
 	{
-		while (d2 <= 9)
-		{
-			putchar(d1 + '0');
-			putchar(',' + ' ');
-			putchar(d2 + '0');
-			d2++;
-		}
+		putchar(d1 + '0');
+		putchar(',');
+		putchar(' ');
 		d1++;
-		d2 = 0;
 	}
-	putchar('\n');
 	return (0);
 }
