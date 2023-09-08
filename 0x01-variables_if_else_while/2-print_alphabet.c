475727d9c1f1012592650b83b05a7372ad8d1513
AbdullahArmiyao <abdulbnarmi@gmail.com>
2023-09-08 09:50:30 -0700

    printing alphabets in lowercase using putchar

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main -Entry point
+ * Return: (0) success
+ */
+
+int main(void)
+{
+	char chs[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l'}
+	char ch[] = {'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'}
+
+	putchar(chs);
+	putchar(ch);
+	return (0);
+}
