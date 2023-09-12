3e5577bed06b487344e2c956fbde915a5ef6071c
AbdullahArmiyao <abdulbnarmi@gmail.com>
2023-09-12 07:53:37 -0700

    printalphabets with function defined in main.h

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - Entry Point
+ * Return: (0) success
+ */
+
+void print_alphabet(void)
+{
+	for(int i = a; i <=z; i++)
+	{
+		_putchar(i);
+	}
+}
+
+int main(void)
+{
+	print_alphabet();
+	_putchar('\n');
+	return(0)
+}
