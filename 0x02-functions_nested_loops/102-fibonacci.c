21442138bf3d2dd135773dde889642ac3e7e0a72
AbdullahArmiyao <abdulbnarmi@gmail.com>
2023-09-13 07:46:02 -0700

    printing fibonacci numbers < 50

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main -Entry point
+ *
+ * Return: void
+ *
+ */
+
+int main(void)
+{
+	int c, f1, f2, s;
+
+	for (c = 0; c < 50; c++)
+	{
+		s = f1 + f2;
+		printf("%d", s);
+
+		f1 = f2;
+		f2 = sum;
+
+		if (c == 49)
+			printf("\n");
+		else
+			printf(", ");
+
+	return (0);
+}
