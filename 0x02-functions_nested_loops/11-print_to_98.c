bc493f1bc92c91eb125ae25b328f8caf2b9d684f
AbdullahArmiyao <abdulbnarmi@gmail.com>
2023-09-12 21:52:55 -0700

    printing numbers to 98 from any integer

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -15,9 +15,13 @@ void print_to_98(int n)
 	{
 		int i;
 
-		for (i = n; i == 98; i++)
+		for (i = n; i <= 98; i++)
 		{
-			printf("%d, ", i);
+			printf("%d", i);
+			if (i != 98)
+			{
+				printf(", ");
+			}
 		}
 	}
 
@@ -25,9 +29,11 @@ void print_to_98(int n)
 	{
 		int j;
 
-		for (j = n; j == 98; j++)
+		for (j = n; j >= 98; j--)
 		{
 			printf("%d, ", j);
+			if (j != 98)
+				printf(", ");
 		}
 	}
 
