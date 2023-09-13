e0c8e22020c0b7a9c2788d0f47eec5fc62e6fe78
AbdullahArmiyao <abdulbnarmi@gmail.com>
2023-09-12 21:36:14 -0700

    printing to 98 from any integer

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_to_98 - prints to 98
+ *
+ * @n: number to be checked
+ *
+ * Return: void
+ */
+
+void print_to_98(int n)
+{
+	if (n <= 98)
+	{
+		for (int i = n; i <= 98; i++)
+		{
+			printf("%d, ", i);
+		}
+	}
+	else if (n > 98)
+	{
+		for (int j = n; j <= 98; j++)
+		{
+			printf("%d, ", j);
+		}
+	}
+	printf("\n");
+}
