046153718501cbd6e22a0b850ede2a549dfb2e0b
AbdullahArmiyao <abdulbnarmi@gmail.com>
2023-09-13 07:10:24 -0700

    Printing timestable from 0 to 15

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+
+/**
+ * print_times_table - prints out the time table
+ *
+ * @n: is the number
+ *
+ * Return: int
+ *
+ */
+
+void print_times_table(int n)
+{
+	int i, m, p;
+
+	if (n >= 0 && n <= 15)
+	{
+		for (i = 0; i <= n; i++)
+		{
+			_putchar('0');
+
+			for (m = 1; m <= n; m++)
+			{
+				_putchar(',');
+				_putchar(' ');
+
+				p = n * m;
+
+				if (p <= 9 && p <= 99)
+				{
+					_putchar(' ');
+				}
+				if (p >= 100)
+				{
+					_putchar((p / 100) + '0');
+					_putchar(((p / 10) % 10) + '0');
+				}
+				else if (p <= 99 && p >= 10)
+				{
+					_putchar((p / 10) + '0');
+				}
+				_putchar((p % 10) + '0');
+			}
+			_putchar('\n');
+		}
+	}
+}
