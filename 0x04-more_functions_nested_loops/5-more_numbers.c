ac741892a17e1716943d6807325ddd96285c41ca
AbdullahArmiyao <abdulbnarmi@gmail.com>
2023-09-14 12:30:20 -0700

    printing numbers x10

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * more_numbers - main function
+ *
+ * Return: void
+ */
+
+void more_numbers(void)
+{
+	int i = 0;
+	int j = 0;
+
+	while (i <= 10)
+	{
+		j = 0;
+		while (j <= 14)
+		{
+			_putchar(j + '0');
+			j++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
