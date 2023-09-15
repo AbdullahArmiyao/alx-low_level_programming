c6d8a368d1505dd69b4c0bac8679fcca7dc3dceb
AbdullahArmiyao <abdulbnarmi@gmail.com>
2023-09-14 19:57:46 -0700

    ignore

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,17 +8,20 @@
 
 void more_numbers(void)
 {
-	int i, j;
+	int i, j, k;
 
-	for (i = 0; i < 11; i++)
+	for (i = 0; i < 10; i++)
 	{
-		for (j = 0; j < 15; j++)
+		for (j = 0; j <= 14; j++)
 		{
+			if ( j <= 9)
+				k = j;
 			if (j > 9)
 			{
 				_putchar((j / 10) + '0');
+				k = j  % 10;
 			}
-			_putchar ((j % 10) + '0');
+			_putchar(k + '0');
 		}
 		_putchar('\n');
 	}
