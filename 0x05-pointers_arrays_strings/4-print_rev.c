b33da13d9e213edda300c6c9c020b54745b6ca46
AbdullahArmiyao <abdulbnarmi@gmail.com>
2023-09-18 04:42:15 -0700

    reversing a string

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * print_rev - main function
+ *
+ * @s: value to be reversed
+ *
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+	puts(strrev(s));
+}
