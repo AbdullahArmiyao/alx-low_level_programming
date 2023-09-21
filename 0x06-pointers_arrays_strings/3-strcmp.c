8034a7d32ea855c8e8881abae1dcdc4cce480206
AbdullahArmiyao <abdulbnarmi@gmail.com>
2023-09-21 13:23:42 -0700

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -12,25 +12,13 @@
 char _strcmp(char *s1, char *s2)
 {
 
-	while (*s1 != '\0' && *s2 != '\0')
+	while (*s1 == *s2)
 	{
-		if (*s1 < *s2)
-			return (-1);
-		else if (*s1 > *s2)
-			return (1);
+		if (*s1 == '\0')
+			return (0);
 		s1++;
 		s2++;
 	}
 
-	if (*s1 == '\0' && *s2 != '\0')
-	{
-		return (-1);
-	}
-
-	else if (*s1 != '\0' && *s2 == '\0')
-	{
-		return (1);
-	}
-
-	return (0);
+	return (*s1 - *s2);
 }
