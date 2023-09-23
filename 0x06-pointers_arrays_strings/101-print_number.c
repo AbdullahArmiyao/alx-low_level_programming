698212afa0cb47da839415bbb443d21360eb0dde
AbdullahArmiyao <abdulbnarmi@gmail.com>
2023-09-23 14:49:07 -0700

    print integer

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_number - function that prints an integer
+ *
+ * @n: integer
+ *
+ * Return: void
+ */
+
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	if (n / 10 != 0)
+	{
+		_putchar(n / 10 + '0');
+	}
+	_putchar(n % 10 + '0');
+}
