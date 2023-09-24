29b3b337244007058ed27a040ae1e0b683a113df
AbdullahArmiyao <abdulbnarmi@gmail.com>
2023-09-23 21:43:57 -0700

    print number

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -12,14 +12,20 @@ void print_number(int n)
 {
 	if (n < 0)
 	{
+		n *= -1;
 		_putchar('-');
-		n = -n;
+		//n = -n;
 	}
 
-	if (n / 10 != 0)
+	int i = n;
+
+	//if (n / 10 != 0)
+	if (n / 10)
 	{
-		_putchar(n / 10 + '0');
+		//_putchar(n / 10 + '0');
+		print_number(n / 10);
 	}
 
-	_putchar(n % 10 + '0');
+	//_putchar(n % 10 + '0');
+	_putchar(i % 10 + '0');
 }
