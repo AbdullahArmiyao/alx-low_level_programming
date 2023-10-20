36c74d37d58f95c68e404061593230b80a1f92ae
AbdullahArmiyao <abdulbnarmi@gmail.com>
2023-10-19 18:04:50 -0700

    function that frees a list_t list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+
+/**
+ * safely_free_mem - safely free allocated memory
+ * @pnt: pointer to memory location
+ */
+
+void safely_free_mem(void **pnt)
+{
+	if (pnt != NULL || *pnt != NULL)
+	{
+		free(*pnt);
+		pnt = NULL;
+	}
+}
+
+/**
+ * free_list - frees a list_t list
+ * @head: pointer to head node
+ */
+
+void free_list(list_t *head)
+{
+	list_t *c_node;
+
+	while (head != NULL)
+	{
+		c_node = head;
+		head = head->next;
+		safely_free_mem(c_node->str);
+		safely_free_mem(c_node);
+	}
+}
